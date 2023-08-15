100da6008d763fe228f3aa20c80c2cac263b7a6a
susantapaul01 <boxme.sp@gmail.com>
2023-08-16 00:29:43 +0600

    not change

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -2,19 +2,27 @@
 import blogData from "../../data/blogData";
 
 const singleBlog = ({params}) => {
-  const id = params.id;
-    // let post = blogData.find(({id}) => (id === params.id));
-    console.log(id);
-  return (
-    <section>
-        <div className="border border-slate-500 p-2 rounded">
-          
-            <h1 className="mb-[15px]">Blog Id: {blogData[id].id}</h1>
-            <h2 className="mb-[15px]">The Title: {blogData[id].title}</h2>
-            <p className="">The Body: {blogData[id].body}</p>
-        </div>
-    </section>
-  )
+  const i = parseInt(params.id);
+  const post = blogData.find((item) => item.id === i);
+  console.log("Hello")
+  // console.log(post.title);
+  if(!post) {
+    return <div>No data found</div>
+  }
+  else {
+    return (
+      <section>
+          <div className="border border-slate-500 p-2 rounded">
+              {/* <h1 className="mb-[15px]">Blog Id: {blogData[i].id}</h1>
+              <h2 className="mb-[15px]">The Title: {blogData[i].title}</h2>
+              <p className="">The Body: {blogData[i].body}</p> */}
+              <h1 className="mb-[15px]">Blog Id: {post.id}</h1>
+              <h2 className="mb-[15px]">The Title: {post.title}</h2>
+              <p className="">The Body: {post.body}</p>
+          </div>
+      </section>
+    )
+  }
 }
 
 export default singleBlog;
