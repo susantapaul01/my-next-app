3d558852987b5e2811794eb8cb4ba99acb431898
susantapaul01 <boxme.sp@gmail.com>
2023-08-15 23:49:33 +0600

    add json file

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -2,17 +2,16 @@
 import blogData from "../../data/blogData";
 
 const singleBlog = ({params}) => {
-    let post = blogData.find((id) => (id === params.id));
-    console.log(post)
-
+  const id = params.id;
+    // let post = blogData.find(({id}) => (id === params.id));
+    console.log(id);
   return (
     <section>
         <div className="border border-slate-500 p-2 rounded">
           
-            {/* <h1>Blog Id: {post.id}</h1> */}
-            <h1 className="mb-[20px] font-bold"> The title: </h1>
-            <h2 className="">The Body:</h2>
-            <div className=""></div>
+            <h1 className="mb-[15px]">Blog Id: {blogData[id].id}</h1>
+            <h2 className="mb-[15px]">The Title: {blogData[id].title}</h2>
+            <p className="">The Body: {blogData[id].body}</p>
         </div>
     </section>
   )
